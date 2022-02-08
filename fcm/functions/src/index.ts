b8ba6e07bbeddb7fbffa5023ddcc3a6b3e1e7f66
Henrik Johansson <hnrkj99@gmail.com>
2022-02-08 23:17:09 +0100

    Added FCM to repo. Might as well keep it here.

---
+++ fcm/functions/src/index.ts
@@ -0,0 +1,32 @@
+import * as functions from "firebase-functions";
+import { initializeApp } from "firebase-admin/app";
+import { getMessaging } from "firebase-admin/messaging";
+
+initializeApp();
+
+const topic = "triggerValuationAlarmWorker";
+const message = {
+  data: {
+    trigger: "true",
+  },
+  topic,
+};
+
+exports.triggerWorkers = functions
+  .region("europe-west1")
+  .pubsub
+  .schedule("*/30 08-18 * * 1-5")
+  .timeZone("Europe/Stockholm")
+  .onRun((context) => {
+    functions.logger.info("Triggered schedule... Executing", context);
+    getMessaging()
+      .send(message)
+      .then((response) => {
+        functions.logger.info("Successfully sent message", response);
+      })
+      .catch((error) => {
+        functions.logger.error("Error sending message", error);
+      });
+
+    return null;
+  });
