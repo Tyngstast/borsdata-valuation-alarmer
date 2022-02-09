c96ff634adf2e45b193649619a4b39dc43bd5553
Henrik Johansson <hnrkj99@gmail.com>
2022-02-09 19:39:04 +0100

    Hide apiKey by default and enable toggle visibility.

--- androidApp/src/main/java/com/github/tyngstast/borsdatavaluationalarmer/android/ui/login/Login.kt
+++ androidApp/src/main/java/com/github/tyngstast/borsdatavaluationalarmer/android/ui/login/Login.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
@@ -15,6 +16,8 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -27,6 +30,8 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.flowWithLifecycle
 import kotlinx.coroutines.flow.Flow
@@ -41,12 +46,14 @@ fun Login(
     val keyboardController = LocalSoftwareKeyboardController.current
 
     var apiKey: String by remember { mutableStateOf("") }
+    var apiKeyVisibility: Boolean by remember { mutableStateOf(false) }
     var successCalled: Boolean by remember { mutableStateOf(false) }
 
     val lifecycleOwner = LocalLifecycleOwner.current
-    val lifecycleAwareAlarmsFlow: Flow<ApiKeyState> = remember(viewModel.apiKeyState, lifecycleOwner) {
-        viewModel.apiKeyState.flowWithLifecycle(lifecycleOwner.lifecycle)
-    }
+    val lifecycleAwareAlarmsFlow: Flow<ApiKeyState> =
+        remember(viewModel.apiKeyState, lifecycleOwner) {
+            viewModel.apiKeyState.flowWithLifecycle(lifecycleOwner.lifecycle)
+        }
 
     @SuppressLint("StateFlowValueCalledInComposition")
     val state by lifecycleAwareAlarmsFlow.collectAsState(viewModel.apiKeyState.value)
@@ -84,12 +91,22 @@ fun Login(
             OutlinedTextField(
                 value = apiKey,
                 onValueChange = onChange,
+                visualTransformation = if (apiKeyVisibility) VisualTransformation.None else PasswordVisualTransformation(),
                 isError = state.error != null,
                 trailingIcon = {
-                    if (state.loading) {
-                        CircularProgressIndicator(modifier = Modifier.scale(0.5F))
-                    } else if (state.error != null) {
-                        Icon(Icons.Filled.Info, "Fel", tint = MaterialTheme.colors.error)
+                    when {
+                        state.loading -> CircularProgressIndicator(modifier = Modifier.scale(0.5F))
+                        state.error != null -> Icon(
+                            Icons.Default.Info,
+                            "Fel",
+                            tint = MaterialTheme.colors.error
+                        )
+                        else -> IconButton(onClick = { apiKeyVisibility = !apiKeyVisibility }) {
+                            Icon(
+                                if (apiKeyVisibility) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                "Toggla API-nyckel synlighet"
+                            )
+                        }
                     }
                 },
                 label = { Text("API-nyckel") },
