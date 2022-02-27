4839d3a9c6c8ad33d554513eea162f1ee6fb1e4d
Henrik Johansson <hnrkj99@gmail.com>
2022-02-27 12:13:35 +0100

    Moved to montSerrat font.

--- androidApp/src/main/java/com/github/tyngstast/borsdatavaluationalarmer/android/ui/theme/Typography.kt
+++ androidApp/src/main/java/com/github/tyngstast/borsdatavaluationalarmer/android/ui/theme/Typography.kt
@@ -1,8 +1,20 @@
 package com.github.tyngstast.borsdatavaluationalarmer.android.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.github.tyngstast.borsdatavaluationalarmer.android.R
+
+val montSerrat = FontFamily(
+    Font(R.font.montserrat_regular, weight = FontWeight.Normal),
+    Font(R.font.montserrat_medium, weight = FontWeight.Bold),
+    Font(R.font.montserrat_light, weight = FontWeight.Light),
+    Font(R.font.montserrat_thin, weight = FontWeight.Thin),
+    Font(R.font.montserrat_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+)
 
 val Typography = Typography(
-    defaultFontFamily = FontFamily.Monospace,
+    defaultFontFamily = montSerrat
 )
