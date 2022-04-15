825dcee13bc44364ffd8e7549ec89362fbb826a2
Henrik Johansson <hnrkj99@gmail.com>
2022-04-15 13:51:03 +0200

    Bump version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.github.tyngstast.borsdatavaluationalarmer.android"
         minSdk = 23
         targetSdk = 32
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
     }
     signingConfigs {
         create("release") {
