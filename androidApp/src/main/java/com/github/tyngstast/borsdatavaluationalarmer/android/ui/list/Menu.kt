d0ed1958c344a6bc6facf3a6508005b2db1694e9
Henrik Johansson <hnrkj99@gmail.com>
2022-04-15 14:44:28 +0200

    Added menu button to leave feedback.

--- androidApp/src/main/java/com/github/tyngstast/borsdatavaluationalarmer/android/ui/list/Menu.kt
+++ androidApp/src/main/java/com/github/tyngstast/borsdatavaluationalarmer/android/ui/list/Menu.kt
@@ -1,5 +1,8 @@
 package com.github.tyngstast.borsdatavaluationalarmer.android.ui.list
 
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.DropdownMenu
@@ -16,10 +19,29 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun Menu(onResetKey: () -> Unit) {
     var expanded by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val feedbackEmailIntent: () -> Unit = {
+        val email = "bd.varderingslarm@gmail.com"
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:$email")
+            putExtra(Intent.EXTRA_SUBJECT, "Feedback")
+        }
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        } else {
+            Toast.makeText(
+                context,
+                "Hittade ingen app för e-post.\r\n\nMaila oss på:\r\n$email",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 
     Box(
         modifier = Modifier
@@ -32,6 +54,9 @@ fun Menu(onResetKey: () -> Unit) {
             expanded = expanded,
             onDismissRequest = { expanded = false },
         ) {
+            DropdownMenuItem(onClick = feedbackEmailIntent) {
+                Text("Lämna Feedback")
+            }
             DropdownMenuItem(onClick = onResetKey) {
                 Text("Nollställ API-nyckel")
             }
