0d0b1e05c6ef853ddd402aabd9d7cc58a0e6e9a3
Henrik Johansson <hnrkj99@gmail.com>
2022-04-25 23:42:37 +0200

    IOS theme styling
      * Added colors and font, same as android values.
      * Styled status bar and navigation for light mode.
      * First iteration LoginView.
        * Still need to handle errors, do localization and fix some input bugs.

---
+++ iosApp/iosApp/Views/AppRootView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+/// By wrapping views in a RootView, they will become the app's main / primary view. This will enable setting the statusBarStyle.
+struct RootView<Content: View>: View {
+    var content: Content
+    
+    init(@ViewBuilder content: () -> (Content)) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        EmptyView()
+            .onAppear {
+                UIApplication.shared.setHostingController(rootView: AnyView(content))
+            }
+    }
+}
+
+/// Set theme customizations on app root view
+struct AppRootView<Content: View>: View {
+    var content: Content
+    
+    init(@ViewBuilder content: () -> (Content)) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        RootView {
+            content
+        }
+        .font(Font.custom(.appFont, size: 17))
+        .statusBarStyle(.lightContent)
+        .navigationBarStyle(font: .navigationTitleFont, backgroundColor: .primaryColor, titleColor: .white)
+        
+    }
+}
+
