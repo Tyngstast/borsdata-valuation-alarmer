124c2166fffabc080c19fc5cb6fd10e224cf8ff8
Henrik Johansson <hnrkj99@gmail.com>
2022-04-26 22:48:35 +0200

    Continued loginView.
      * Moved font to content in RootView, seems to have fixed bugg where it did not apply correctly.
      * loading = false on other states.
      * submit verify key on paste
      * ProgressView on loading.
      * disable submit button on known invalid input.
      * Disabled styling.

--- iosApp/iosApp/Views/LoginView.swift
+++ iosApp/iosApp/Views/LoginView.swift
@@ -8,7 +8,8 @@ struct LoginView: View {
         LoginViewContent(
             loading: viewModel.loading,
             error: viewModel.error,
-            onVerify: { viewModel.verifyKey($0) }
+            onVerify: { viewModel.verifyKey($0) },
+            onClearErrror: { viewModel.clearError() }
         )
     }
 }
@@ -17,33 +18,34 @@ struct LoginViewContent: View {
     var loading: Bool
     var error: ErrorCode?
     var onVerify: (String) -> Void
+    var onClearErrror: () -> Void
     
+    // Keep track of previous value to detect paste input
+    @State var prevValue = ""
     @State var apiKey = ""
+    
+    var loginDisabled: Bool {
+        return apiKey.count < 20 || loading || error != nil
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
-            SecureInputField("API Key", text: $apiKey)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-                .padding()
-                .frame(height: 60)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5.0)
-                        .strokeBorder(
-                            Color.gray,
-                            style: StrokeStyle(lineWidth: 2.0)
-                        )
-                )
-                .background(Color.backgroundColor)
+            SecureInputField("API Key", loading: loading, text: $apiKey)
+                .onChange(of: apiKey) { value in
+                    onClearErrror()
+                    if (value.count - 2 > prevValue.count) {
+                        onVerify(apiKey)
+                    }
+                    prevValue = value
+                }
             if (error != nil) {
                 HStack {
                     Image(systemName: "exclamationmark.circle.fill")
-                    Text("Error")
+                    Text(error!.resourceId)
                 }
                 .foregroundColor(.errorColor)
             }
             Button(action: {
-                // validate input
                 guard !apiKey.isEmpty else {
                     return
                 }
@@ -51,12 +53,12 @@ struct LoginViewContent: View {
             }, label: {
                 Text("VERIFY")
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
-                    .background(Color.primaryColor)
-                    .foregroundColor(.white)
+                    .background(loginDisabled ? Color.gray : Color.primaryColor)
+                    .foregroundColor(loginDisabled ? .paleWhite : .white)
                     .cornerRadius(5.0)
                     .padding(.top, 7.5)
             })
-            
+            .disabled(loginDisabled)
         }
         .navigationTitle("Requires Börsdata Pro")
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
@@ -67,10 +69,12 @@ struct LoginViewContent: View {
 struct SecureInputField: View {
     @Binding private var text: String
     @State private var isSecured: Bool = true
+    private var loading: Bool
     private var title: String
     
-    init(_ title: String, text: Binding<String>) {
+    init(_ title: String, loading: Bool, text: Binding<String>) {
         self.title = title
+        self.loading = loading
         self._text = text
     }
     
@@ -81,27 +85,47 @@ struct SecureInputField: View {
                     SecureField(title, text: $text)
                 } else {
                     TextField(title, text: $text)
+                        .keyboardType(.alphabet)
                 }
-            }.padding(.trailing, 32)
-
+            }
+            .padding()
+            .disableAutocorrection(true)
+            .autocapitalization(.none)
+            .frame(height: 60)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5.0)
+                    .strokeBorder(
+                        Color.gray,
+                        style: StrokeStyle(lineWidth: 2.0)
+                    )
+            )
+            .background(Color.backgroundColor)
+            
             Button(action: {
                 isSecured.toggle()
             }) {
-                Image(systemName: self.isSecured ? "eye.slash" : "eye")
-                    .accentColor(.gray)
+                if loading {
+                    ProgressView()
+                        .accentColor(.gray)
+                } else {
+                    Image(systemName: self.isSecured ? "eye.slash.fill" : "eye.fill")
+                        .accentColor(.gray)
+                }
             }
+            .padding(.trailing, 16)
         }
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            LoginViewContent(
-                loading: false,
-                error: nil,
-                onVerify: { _ in }
-            ).previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
+            NavigationView {
+                LoginViewContent(
+                    loading: false,
+                    error: nil,
+                    onVerify: { _ in },
+                    onClearErrror: { }
+                ).previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+            }
     }
 }
