34a03bca57698e148de412731a55337bf86e0f2a
Henrik Johansson <hnrkj99@gmail.com>
2022-05-07 11:52:32 +0200

    Moved to ios 15.1 target and fixed focus on inputfield when toggling between secureField and textField. Still has a bug where toggling between them and entering new input clears the SecureField.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -30,7 +30,7 @@ kotlin {
     cocoapods {
         summary = "Some description for the Shared Module"
         homepage = "Link to the Shared Module homepage"
-        ios.deploymentTarget = "14.1"
+        ios.deploymentTarget = "15.1"
         podfile = project.file("../iosApp/Podfile")
         framework {
             isStatic = false // SwiftUI preview requires dynamic framework
